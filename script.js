c61b54c720b3997888798900fb08cbba6f23b829
Filza Ahmed <70163799+nfahmed11@users.noreply.github.com>
2022-05-10 22:14:20 -0500

    Add files via upload
    nav bar update

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-let contactBox = document.querySelector(".box");
-let animateBox = "animate__bounceInUp";
-
-contactBox.addEventListener("mouseover", function (e) {
-  contactBox.classList.add("animate__bounceInUp");
-});
-
-contactBox.addEventListener("mouseout", function (e) {
-  contactBox.classList.remove("animate__bounceInUp");
-  contactBox.classList.add("animate__bounceOutDown");
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll > 300) {
+      $("#header").css("background", "rgb(24, 42, 78)");
+      $("#header h3 a").css("color", "rgba(255, 255, 255, 0.95)");
+      $(" #header nav ul li a").css("color", "rgba(255, 255, 255, 0.95)");
+    } else {
+      $("#header").css("background", "rgba(255, 255, 255, 0.95)");
+      $("#header h3 a").css("color", "rgb(24, 42, 78)");
+      $(" #header nav ul li a").css("color", "rgb(24, 42, 78)");
+    }
+  });
 });
